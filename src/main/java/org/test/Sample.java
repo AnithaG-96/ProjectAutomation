7d56d3a62e46dabfb96e3869d68bcd02f8cc4209
AnithaG-96 <anithagk96@gmail.com>
2022-03-15 10:44:38 +0530

    firstcodes

---
+++ src/main/java/org/test/Sample.java
@@ -0,0 +1,72 @@
+package org.test;
+
+public class Sample {
+	private void test1() {
+System.out.println("Test A1");
+	}
+	
+	private void test2() {
+		System.out.println("Test A2");
+			}
+			
+			
+
+	private void test3() {
+		System.out.println("Test A3");
+			}
+			
+		
+	
+	
+	
+public static void main(String[] args) {
+  Sample s=new Sample();
+  s.test1();
+  s.test2();
+  s.test3();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
